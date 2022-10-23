99fa41bad43d15a2652e27e3a12c36b004a0e068
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 11:10:10 -0300

    Test: Add employee with name test

--- CalculadoraDeSalario/Employee/test_employee.py
+++ CalculadoraDeSalario/Employee/test_employee.py
@@ -42,3 +42,10 @@ def test_create_invalid_role_employee():
 
     with pytest.raises(Exception):
         invalid_role_employee = Employee(invalid_role)
+
+
+def test_create_employee_with_name():
+    name = "Jarbas"
+    named_employee = Employee(name=name)
+
+    assert named_employee == name
