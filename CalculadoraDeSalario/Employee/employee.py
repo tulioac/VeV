aa2b5a847571480ee9e70c41d698dff290d6bd69
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 11:13:08 -0300

    Fix: Add name attribute to Employee

--- CalculadoraDeSalario/Employee/employee.py
+++ CalculadoraDeSalario/Employee/employee.py
@@ -2,8 +2,9 @@ ALLOWED_ROLES = ["Developer", "DBA", "Tester", "Manager"]
 
 
 class Employee:
-    def __init__(self, role="Developer") -> None:
+    def __init__(self, role="Developer", name="") -> None:
         if role not in ALLOWED_ROLES:
             raise SystemError(f"{role} is not a valid role for employee")
 
         self.role = role
+        self.name = name
