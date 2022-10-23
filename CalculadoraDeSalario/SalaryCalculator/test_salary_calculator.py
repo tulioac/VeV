7fdb6b9ba94ce5f2371a1167be0eb029e391a086
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 11:42:08 -0300

    Refact: Reuse Salary Calculator instance

--- CalculadoraDeSalario/SalaryCalculator/test_salary_calculator.py
+++ CalculadoraDeSalario/SalaryCalculator/test_salary_calculator.py
@@ -1,23 +1,20 @@
 from ..Employee.employee import Employee
 from .salaryCalculator import SalaryCalculator
 
+salaryCalculator = SalaryCalculator()
+
 
 def test_exist_salary_calculator():
-    salaryCalculator = SalaryCalculator()
-
     assert salaryCalculator != None
 
 
 def test_exist_calculate():
     employee = Employee()
 
-    salaryCalculator = SalaryCalculator()
     salaryCalculator.calculate(employee)
 
 
 def test_calculate_return():
     employee = Employee()
-
-    salaryCalculator = SalaryCalculator()
 
     assert salaryCalculator.calculate(employee) == 0
