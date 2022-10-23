2b283687893d932b349d6065f7b7404506e6f344
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 12:16:40 -0300

    Fix: Add base DBA liquid salary calculation

--- CalculadoraDeSalario/SalaryCalculator/salaryCalculator.py
+++ CalculadoraDeSalario/SalaryCalculator/salaryCalculator.py
@@ -3,15 +3,24 @@ from ..Employee.employee import Employee
 
 class SalaryCalculator:
     def calculate(self, employee: Employee) -> float:
-        discount_value = 0.2
-
-        salary_threshold = 3000
-
+        employee_role = employee.role
         employee_base_salary = employee.base_salary
 
-        if employee_base_salary < salary_threshold:
-            discount_value = 0.1
+        if employee_role == "Developer":
+            discount_value = 0.2
 
-        liquid_salary = employee_base_salary * (1 - discount_value)
+            salary_threshold = 3_000
 
-        return liquid_salary
+            if employee_base_salary < salary_threshold:
+                discount_value = 0.1
+
+            liquid_salary = employee_base_salary * (1 - discount_value)
+
+            return liquid_salary
+
+        else:
+            discount_value = 0.25
+
+            liquid_salary = employee_base_salary * (1 - discount_value)
+
+            return liquid_salary
