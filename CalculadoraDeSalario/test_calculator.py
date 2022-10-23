51c0172cfcc1ca8e05d5f846c7cecdadea1bfe41
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 10:09:14 -0300

    Test: Add exist employee class test

--- CalculadoraDeSalario/test_calculator.py
+++ CalculadoraDeSalario/test_calculator.py
@@ -5,3 +5,9 @@ def test_exist_salary_calculator():
     salaryCalculator = SalaryCalculator()
 
     assert salaryCalculator != None
+
+
+def test_exist_employee():
+    employee = Employee()
+
+    assert employee != None
