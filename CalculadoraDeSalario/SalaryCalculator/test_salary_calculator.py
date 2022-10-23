bae3968fccc0355046bd014663cf059f9867315a
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 11:59:53 -0300

    Refact: Add variable to store base salary

--- CalculadoraDeSalario/SalaryCalculator/test_salary_calculator.py
+++ CalculadoraDeSalario/SalaryCalculator/test_salary_calculator.py
@@ -21,8 +21,10 @@ def test_calculate_return():
 
 
 def test_calculate_developer_salary_over_3000():
-    employee = Employee(role="Developer", base_salary=5_000)
+    base_salary = 5_000
 
-    developer_liquid_salary = 5_000 * 0.8
+    employee = Employee(role="Developer", base_salary=base_salary)
+
+    developer_liquid_salary = base_salary * 0.8
 
     assert developer_liquid_salary == salaryCalculator.calculate(employee)
