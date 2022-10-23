3472e6d54bdfd8eacef2d3af99b085f6c646f26a
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 11:19:13 -0300

    Fix: Add base salary attribute to Employee

--- CalculadoraDeSalario/Employee/employee.py
+++ CalculadoraDeSalario/Employee/employee.py
@@ -2,10 +2,11 @@ ALLOWED_ROLES = ["Developer", "DBA", "Tester", "Manager"]
 
 
 class Employee:
-    def __init__(self, role="Developer", name="", email="") -> None:
+    def __init__(self, role="Developer", name="", email="", base_salary=0) -> None:
         if role not in ALLOWED_ROLES:
             raise SystemError(f"{role} is not a valid role for employee")
 
         self.role = role
         self.name = name
         self.email = email
+        self.base_salary = base_salary
