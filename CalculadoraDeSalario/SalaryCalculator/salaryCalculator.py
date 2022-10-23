a931678d57641363619586800fd26887271176a4
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 10:32:51 -0300

    Docs: Move system specification to README

--- CalculadoraDeSalario/SalaryCalculator/salaryCalculator.py
+++ CalculadoraDeSalario/SalaryCalculator/salaryCalculator.py
@@ -1,11 +1,3 @@
-# Deve-se implementar uma calculadora de salário de funcionários. Um funcionário contém nome, e-mail, salário-base e cargo. De acordo com seu cargo, a regra para cálculo do salário líquido é diferente:
-
-# Caso o cargo seja DESENVOLVEDOR, o funcionário terá desconto de 20% caso o salário seja maior ou igual que 3.000,00, ou apenas 10% caso o salário seja menor que isso.
-# Caso o cargo seja DBA, o funcionário terá desconto de 25% caso o salário seja maior ou igual que 2.000,00, ou apenas 15% caso o salário seja menor que isso.
-# Caso o cargo seja TESTADOR, o funcionário terá desconto de 25% caso o salário seja maior ou igual que 2.000,00, ou apenas 15% caso o salário seja menor que isso.
-# Caso o cargo seja GERENTE, o funcionário terá desconto de 30% caso o salário seja maior ou igual que 5.000,00, ou apenas 20% caso o salário seja menor que isso.
-
-
 from ..Employee.employee import Employee
 
 
