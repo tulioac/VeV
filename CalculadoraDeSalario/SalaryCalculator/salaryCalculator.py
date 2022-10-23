2f207659cc1dcd863e992b1d8e46987d15338408
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 12:04:58 -0300

    Fix: Add variable salary reduction based on base salary value

--- CalculadoraDeSalario/SalaryCalculator/salaryCalculator.py
+++ CalculadoraDeSalario/SalaryCalculator/salaryCalculator.py
@@ -3,4 +3,15 @@ from ..Employee.employee import Employee
 
 class SalaryCalculator:
     def calculate(self, employee: Employee) -> float:
-        return employee.base_salary * 0.8
+        discount_value = 0.2
+
+        salary_threshold = 3000
+
+        employee_base_salary = employee.base_salary
+
+        if employee_base_salary < salary_threshold:
+            discount_value = 0.1
+
+        liquid_salary = employee_base_salary * (1 - discount_value)
+
+        return liquid_salary
