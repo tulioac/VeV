ed87ebf504c55eeca3250914d0a5627132e49107
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 14:42:12 -0300

    Refact: Create private function to calculate liquid salary

--- CalculadoraDeSalario/SalaryCalculator/salaryCalculator.py
+++ CalculadoraDeSalario/SalaryCalculator/salaryCalculator.py
@@ -2,42 +2,55 @@ from ..Employee.employee import Employee
 
 
 class SalaryCalculator:
+    def __calculate_liquid_salary(
+        self,
+        base_salary: float,
+        salary_threshold: float,
+        higher_discount: float,
+        lower_discount: float,
+    ) -> float:
+        discount_value = (
+            lower_discount if base_salary < salary_threshold else higher_discount
+        )
+
+        return base_salary * (1 - discount_value)
+
     def calculate(self, employee: Employee) -> float:
         employee_role = employee.role
         employee_base_salary = employee.base_salary
 
         if employee_role == "Developer":
-            discount_value = 0.2
-
             salary_threshold = 3_000
+            higher_discount_value = 0.2
+            lower_discount_value = 0.1
 
-            if employee_base_salary < salary_threshold:
-                discount_value = 0.1
-
-            liquid_salary = employee_base_salary * (1 - discount_value)
-
-            return liquid_salary
+            return self.__calculate_liquid_salary(
+                base_salary=employee_base_salary,
+                salary_threshold=salary_threshold,
+                higher_discount=higher_discount_value,
+                lower_discount=lower_discount_value,
+            )
 
         elif employee_role == "Manager":
-            discount_value = 0.3
-
             salary_threshold = 5_000
+            higher_discount_value = 0.3
+            lower_discount_value = 0.2
 
-            if employee_base_salary < salary_threshold:
-                discount_value = 0.2
-
-            liquid_salary = employee_base_salary * (1 - discount_value)
-
-            return liquid_salary
+            return self.__calculate_liquid_salary(
+                base_salary=employee_base_salary,
+                salary_threshold=salary_threshold,
+                higher_discount=higher_discount_value,
+                lower_discount=lower_discount_value,
+            )
 
         else:
-            discount_value = 0.25
-
             salary_threshold = 2_000
+            higher_discount_value = 0.25
+            lower_discount_value = 0.15
 
-            if employee_base_salary < salary_threshold:
-                discount_value = 0.15
-
-            liquid_salary = employee_base_salary * (1 - discount_value)
-
-            return liquid_salary
+            return self.__calculate_liquid_salary(
+                base_salary=employee_base_salary,
+                salary_threshold=salary_threshold,
+                higher_discount=higher_discount_value,
+                lower_discount=lower_discount_value,
+            )
