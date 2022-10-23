4043d1f22f9fccae16232ebc12e18fc8500246b0
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 11:15:25 -0300

    Fix: Add email attribute to Employee

--- CalculadoraDeSalario/Employee/employee.py
+++ CalculadoraDeSalario/Employee/employee.py
@@ -2,9 +2,10 @@ ALLOWED_ROLES = ["Developer", "DBA", "Tester", "Manager"]
 
 
 class Employee:
-    def __init__(self, role="Developer", name="") -> None:
+    def __init__(self, role="Developer", name="", email="") -> None:
         if role not in ALLOWED_ROLES:
             raise SystemError(f"{role} is not a valid role for employee")
 
         self.role = role
         self.name = name
+        self.email = email
