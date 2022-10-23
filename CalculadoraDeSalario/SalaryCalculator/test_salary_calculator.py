6f58615c6dc2a7fca6dc0ed89f63af4245e3d76f
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 11:55:52 -0300

    Test: Add calculate developer liquid salary over 3000

--- CalculadoraDeSalario/SalaryCalculator/test_salary_calculator.py
+++ CalculadoraDeSalario/SalaryCalculator/test_salary_calculator.py
@@ -18,3 +18,11 @@ def test_calculate_return():
     employee = Employee()
 
     assert salaryCalculator.calculate(employee) == 0
+
+
+def test_calculate_developer_salary_over_3000():
+    employee = Employee(role="Developer", base_salary=5_000)
+
+    developer_liquid_salary = 5_000 * 0.8
+
+    assert developer_liquid_salary == salaryCalculator.calculate(employee)
