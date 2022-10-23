b7695fa8c4d5472f37ea4b308f020f1663da8b6a
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 10:45:19 -0300

    Feat: Add possible role validation

--- CalculadoraDeSalario/Employee/employee.py
+++ CalculadoraDeSalario/Employee/employee.py
@@ -1,3 +1,9 @@
+ALLOWED_ROLES = ["Developer", "DBA"]
+
+
 class Employee:
-    def __init__(self, role="") -> None:
+    def __init__(self, role="Developer") -> None:
+        if role not in ALLOWED_ROLES:
+            raise SystemError(f"{role} is not a valid role for employee")
+
         self.role = role
