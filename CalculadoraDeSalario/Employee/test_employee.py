0bf8da615ab27b6f9b53fa1fc9844162f75d6014
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-10-23 10:34:03 -0300

    Test: Add create developer employee

--- CalculadoraDeSalario/Employee/test_employee.py
+++ CalculadoraDeSalario/Employee/test_employee.py
@@ -5,3 +5,9 @@ def test_exist_employee():
     employee = Employee()
 
     assert employee != None
+
+
+def test_create_developer_employee():
+    developer = Employee("Developer")
+
+    assert developer.role == "Developer"
